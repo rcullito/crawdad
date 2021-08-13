e7c6c23a5d4c56e050e74faeac02e775f281c7d9
Rob Culliton <rob.culliton@gmail.com>
2021-08-13 16:42:37 -0400

    a helper fn built on cadr

--- crawdad.scm
+++ crawdad.scm
@@ -1,4 +1,13 @@
-(define parts '((nose (deviated-septum breath-right-strips))
+(define anatomy '((nose (deviated-septum breath-right-strips))
                 (tongue (vacuum-activator cpap))
                 (pharyngeal (sleep-tape mad))
                 (inflammation (allergies sugar))))
+
+(define biggest-problem 'tongue)
+
+
+(define (part-fixes part)
+  (cadr (assq part anatomy)))
+
+
+(part-fixes 'inflammation)
