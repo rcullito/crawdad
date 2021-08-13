efa663f6a3748392eafedf91e89c0171c8dedda5
Rob Culliton <rob.culliton@gmail.com>
2021-08-13 16:49:41 -0400

    user-situation

--- crawdad.scm
+++ crawdad.scm
@@ -1,9 +1,15 @@
 (define anatomy '((nose (deviated-septum breath-right-strips))
-                (tongue (vacuum-activator cpap))
-                (pharyngeal (sleep-tape mad))
-                (inflammation (allergies sugar))))
+                  (tongue (vacuum-activator cpap))
+                  (pharyngeal (sleep-tape mad))
+                  (maxilla (agga surgery))
+                  (inflammation (allergies sugar))))
 
-(define biggest-problem 'tongue)
+(define user-situation
+  '((nose ok)
+    (tongue good)
+    (phayngeal bad)
+    (maxilla good)
+    (inflammation ok)))
 
 
 (define (part-fixes part)
