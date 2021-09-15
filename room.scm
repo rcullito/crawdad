6312ca1b68e2a8ec169ac772a8026aa2bcf5b954
Rob Culliton <rob.culliton@gmail.com>
2021-09-15 14:25:17 -0400

    working distinct and list return

--- room.scm
+++ room.scm
@@ -8,9 +8,19 @@
 
 
 (define (multiple-dwelling)
-  (let ((baker (choose 1 2 3 4 5))
-        (cooper (choose 1 2 3 4 5))
-        (fletcher (choose 1 2 3 4 5))
-        (miller (choose 1 2 3 4 5))
-        (smith (choose 1 2 3 4 5)))))
+  (let* ((floors '(1 2 3 4 5))
+        (baker (choose floors))
+        (cooper (choose floors))
+        (fletcher (choose floors))
+        (miller (choose floors))
+        (smith (choose floors)))
+    (if (not (distinct? (list baker cooper fletcher miller smith)))
+        (fail)
+        (list (list 'baker baker)
+              (list 'cooper cooper)
+              (list 'fletcher fletcher)
+              (list 'miller miller)
+              (list 'smith smith)))))
+
+(multiple-dwelling)
 
