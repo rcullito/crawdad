e196edf7bd55950739c11ff08a7ed347a6af618e
Rob Culliton <rob.culliton@gmail.com>
2021-09-15 14:40:35 -0400

    model each constituent potion as a separate choice rather than making just one "correct" choice. then the returned value should be the entire lineup (including the one to move forward)

--- potion.scm
+++ potion.scm
@@ -1,28 +1,11 @@
 (load "nondeterminism.scm")
 
-(define potions '(1 2 3 4 5 6 7))
-
-;; 1.  poison will always be to the left of nettle
-;; 2. the ends will be different, and neither is forward
-
-;; 4. the 2nd in are the same, though different size
-(define potions2 '(poison-1 nettle-1 forward poison-2 poison-3 nettle-2 reverse))
-
-;; the question here is actually how to build the list, not how to choose from it.
-;; look to sussman
+;; 4 clues
+;;
 
 
-;; from the 3 rules so far, we know that it is in the middle 3
-
-
-(eq? (car potions2) 'poison-1)
-;; eventually have 2 choices, harry and hermione
-(let ((choice (choose potions2)))
-  (if (or (eq? choice (car (reverse potions2)))
-          (eq? choice (car potions2))
-          (eq? choice (cadr potions2))
-          (eq? choice (cadr (reverse potions2))))
-      (fail)
-      choice))
-
-
+(let* ((potions '(1 2 3 4 5 6 7))
+       (nettle-1 (choose potions))
+       (nettle-2 (choose potions))
+       (dwarf (choose potions))
+       (giant (choose potions))))
